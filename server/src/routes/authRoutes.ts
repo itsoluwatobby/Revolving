959c20ead57e20f88c77eb7de6859d0f60ae9fe2
itsoluwatobby <oluwasamuel050@gmail.com>
2023-05-10 01:24:52 +0100

    accesstoken middleware

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { accountConfirmation, loginHandler, logoutHandler, registerUser } from "../controller/authController.js";
+import { getNewTokens } from "../middleware/verifyTokens.js";
 
 const authRouter = Router()
 
 authRouter.post('/registration', registerUser);
 authRouter.post('/login', loginHandler);
 authRouter.get('/verify_account', accountConfirmation);
+
+authRouter.get('/new_access_token', getNewTokens);
+
 authRouter.get('/logout', logoutHandler);
 
 export default authRouter
